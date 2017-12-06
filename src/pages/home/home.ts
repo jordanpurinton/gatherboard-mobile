515ad88fa64a87ff8e108317c80106e6830273cd
jordanpurinton <jordanomac@gmail.com>
2017-12-06 14:47:30 -0700

    Add formatter method to handle 24hr time string sent back in API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,7 +31,25 @@ export class HomePage
 
   formatStartTime(startTime)
   {
-    return startTime.substring(0, startTime.length - 3);
+    let firstIndex = parseInt(startTime[0]);
+    let secondIndex = parseInt(startTime[1]);
+    let combined = startTime[0] + startTime[1];
+    let combinedInt = parseInt(combined);
+
+    // 13:00 - 23:59
+    if(combinedInt > 12) {
+      return (combinedInt - 12).toString() + ':' + startTime[3] + startTime[4] + 'p';
+    }
+
+    // 12:00p - 12:59p
+    if(firstIndex == 1 && secondIndex == 2) {
+      return startTime.substring(0, startTime.length - 3) + 'p';
+    }
+
+    // 00:00 - 11:59
+    else {
+      return startTime.substring(0, startTime.length - 3) + 'a';
+    }
   }
 
 }
