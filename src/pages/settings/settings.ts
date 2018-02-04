b86e59c7e7676419fd845b4caf28c021d780f7d4
Ryan Koning <rkoning8@gmail.com>
2018-02-04 13:25:38 -0700

    adding basic storage functionality

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import { Storage } from "@ionic/storage";
 
 @IonicPage()
 @Component({
@@ -7,13 +8,32 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
     templateUrl: 'settings.html',
 })
 export class SettingsPage {
-
-    constructor(public navCtrl: NavController, public navParams: NavParams)
-    {
-    }
+	
+	items: any;
+	
+	constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+		this.storage.get('myStore').then((data) => {
+			console.log('my data'+data);
+			this.items = data;
+		});
+	}
 
     ionViewDidLoad()
     {
     }
+	 
+	save(val) {
+		this.storage.get('storedVar').then((data) => {
+			if (data != null) {
+				data.push (val);
+				this.storage.set('myStore', data);
+			}
+			else {
+				let array = [];
+				array.push(val);
+				this.storage.set('myStore', array);
+			}
+		});
+	}
 
 }
