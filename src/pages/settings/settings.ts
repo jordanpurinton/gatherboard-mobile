8d153dd959c49a5b4ef6b44e5d2023e8f179db9a
Ryan Koning <rkoning8@gmail.com>
2018-02-04 18:52:25 -0700

    added age range, tags and categories selectors

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -9,29 +9,90 @@ import { Storage } from "@ionic/storage";
 })
 export class SettingsPage {
 	
-	items: any;
-	
+	tags: any;
+	categories: Array<string>;
+	knobValues: any = {
+		upper: 50,
+		lower: 0
+	};
 	constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
-		this.storage.get('myStore').then((data) => {
-			console.log('my data'+data);
-			this.items = data;
+		this.storage.get('tags').then((data) => {
+			this.tags = data;
+		});
+		this.categories = new Array<string>();
+		this.categories.push('Sports');
+		this.categories.push('Business');
+		this.categories.push('Education');
+		this.categories.push('Music');
+	}
+	
+	ionViewDidLoad()
+	{
+		
+		var slider = document.getElementById("age-range-slider");
+		console.log(slider);
+		console.log(this.knobValues);
+
+		
+	}
+	
+	saveAgeRange(valLower, valUpper) {
+		console.log(valLower, valUpper);
+		this.storage.get('ageUpper').then((data) => {
+
+				let array = [];
+				array.push(valUpper);
+				this.storage.set('ageUpper', array);
+
+		});
+		this.storage.get('ageLower').then((data) => {
+
+				let array = [];
+				array.push(valLower);
+				this.storage.set('ageLower', array);
+			
 		});
 	}
-
-    ionViewDidLoad()
-    {
-    }
 	 
-	save(val) {
-		this.storage.get('storedVar').then((data) => {
+	saveTag(val) {
+		let tags = val.split(",");
+		this.storage.get('tags').then((data) => {
 			if (data != null) {
-				data.push (val);
-				this.storage.set('myStore', data);
+				for(let k = 0; k < tags.length; k++) {
+					console.log(tags[k]);
+					data.push (tags[k]);
+				}
+				this.storage.set('tags', data);
 			}
 			else {
 				let array = [];
-				array.push(val);
-				this.storage.set('myStore', array);
+				for(let k = 0; k < tags.length; k++) {
+					array.push (tags[k]);
+				}
+				this.storage.set('tags', array);
+			}
+		});
+		
+		for(let k = 0; k < tags.length; k++){
+			this.tags.push(tags[k]);
+		}
+	}
+	
+	saveCategories(vals) {
+			
+			this.storage.get('categories').then((data) => {
+			if (data != null) {
+				for(let k = 0; k < vals.length; k++) {
+					data.push (vals[k]);
+				}
+				this.storage.set('categories', data);
+			}
+			else {
+				let array = [];
+				for(let k = 0; k < vals.length; k++) {
+					array.push (vals[k]);
+				}
+				this.storage.set('categories', array);
 			}
 		});
 	}
