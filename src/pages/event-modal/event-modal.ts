a6ee29c3965658b395b1629f1a41ea2c7f9d5abc
PhillipPeterson <majeh90210@gmail.com>
2018-02-08 01:40:05 -0700

    Set up a basic UI for the event modal

--- src/pages/event-modal/event-modal.ts
+++ src/pages/event-modal/event-modal.ts
@@ -28,4 +28,36 @@ export class EventModalPage {
   {
     return _.unescape(string);
   }
+  
+  formatStartTime(startTime) {
+    let firstIndex = parseInt(startTime[0]);
+    let secondIndex = parseInt(startTime[1]);
+    let combined = startTime[0] + startTime[1];
+    let combinedInt = parseInt(combined);
+
+    // remove trailing 0
+    if (parseInt(startTime[0]) == 0) {
+        startTime = startTime.substring(1, startTime.length + 1);
+    }
+
+    // 13:00 - 23:59
+    if (combinedInt > 12) {
+        return (combinedInt - 12).toString() + ':' + startTime[3] + startTime[4] + 'p';
+    }
+
+    // 12:00p - 12:59p
+    else if (firstIndex == 1 && secondIndex == 2) {
+        return startTime.substring(0, startTime.length - 3) + 'p';
+    }
+
+    // 00:00 - 11:59
+    else {
+        return startTime.substring(0, startTime.length - 3) + 'a';
+    }
+  }
+  
+  formatDate(eventDate){
+    
+  }
+  
 }
