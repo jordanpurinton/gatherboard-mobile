d3b18d5d644ffcbfeaa90556da2c89c7267cadc0
jordanpurinton <jordanomac@gmail.com>
2018-02-09 15:56:24 -0700

    Add class selector.

---
+++ src/app/secret.ts
@@ -0,0 +1,7 @@
+export class Secret {
+    public static uri = 'https://api.gatherboard.com';
+    public static token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTEyOTA1OTQsImp0aSI6IjR4RENiSzNvQzdoVWVPaktDO' +
+        'Go1NUYiLCJzdWIiOiJnYXRoZXJib2FyZCIsInNjb3BlIjpbInRvZG8uYWxsIl0sInNpdGVpZCI6IjlmYzU3NWI2In0.t7WnYCHOmOV_ngK0x630FUAeEO3nhN_PL6jFLyopt8A';
+    public static androidMapKey = 'AIzaSyCGNhkJGtzBEwRi0srNiZsKIbRiu1F-tNY';
+    public static iosMapKey = 'AIzaSyDLTpsRaXzg41owGr6FFkS9805FIe-fzbQ';
+}
