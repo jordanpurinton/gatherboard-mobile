5ea351eb8a101cf3a0acbcceca77105594781673
jordanpurinton <jordanomac@gmail.com>
2018-02-08 21:15:17 -0700

    Include enable location page.

---
+++ src/pages/enable-location/enable-location.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EnableLocationPage } from './enable-location';
+
+@NgModule({
+  declarations: [
+    EnableLocationPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EnableLocationPage),
+  ],
+})
+export class EnableLocationPageModule {}
