814c010d04ba66cad27813aef9fa251aea3dca0e
jordanpurinton <jordanomac@gmail.com>
2018-02-09 17:00:52 -0700

    Add show/hide functionality to event details description.

--- src/pages/event-modal/event-modal.ts
+++ src/pages/event-modal/event-modal.ts
@@ -1,56 +1,65 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as _ from 'he';
 import {IonicPage, NavParams, ViewController, ModalController} from 'ionic-angular';
 import moment from "moment";
 
 @IonicPage()
 @Component({
-  selector: 'page-event-modal',
-  templateUrl: 'event-modal.html',
+    selector: 'page-event-modal',
+    templateUrl: 'event-modal.html',
 })
 export class EventModalPage {
 
-  e = this.navParams.get('e');
+    e = this.navParams.get('e');
+    isExpandedText = false;
 
-  constructor(public viewController: ViewController,
-              public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log(this.e)
-  }
-
-  closeModal()
-  {
-    this.viewController.dismiss();
-  }
-
-  formatStartTime(startTime) {
-    let firstIndex = parseInt(startTime[0]);
-    let secondIndex = parseInt(startTime[1]);
-    let combined = startTime[0] + startTime[1];
-    let combinedInt = parseInt(combined);
-
-    // remove trailing 0
-    if (parseInt(startTime[0]) == 0) {
-        startTime = startTime.substring(1, startTime.length + 1);
+    constructor(public viewController: ViewController,
+                public navParams: NavParams) {
     }
 
-    // 13:00 - 23:59
-    if (combinedInt > 12) {
-        return (combinedInt - 12).toString() + ':' + startTime[3] + startTime[4] + 'p';
+    ionViewDidLoad() {
+        console.log(this.e)
     }
 
-    // 12:00p - 12:59p
-    else if (firstIndex == 1 && secondIndex == 2) {
-        return startTime.substring(0, startTime.length - 3) + 'p';
+    closeModal() {
+        this.viewController.dismiss();
     }
 
-    // 00:00 - 11:59
-    else {
-        return startTime.substring(0, startTime.length - 3) + 'a';
+    expandDescription() {
+        this.isExpandedText = true;
     }
-  }
+
+    collapseDescription() {
+        this.isExpandedText = false;
+    }
+
+    formatStartTime(startTime) {
+        let firstIndex = parseInt(startTime[0]);
+        let secondIndex = parseInt(startTime[1]);
+        let combined = startTime[0] + startTime[1];
+        let combinedInt = parseInt(combined);
+
+        // remove trailing 0
+        if (parseInt(startTime[0]) == 0) {
+            startTime = startTime.substring(1, startTime.length + 1);
+        }
+
+        // 13:00 - 23:59
+        if (combinedInt > 12) {
+            return (combinedInt - 12).toString() + ':' + startTime[3] + startTime[4] + 'p';
+        }
+
+        // 12:00p - 12:59p
+        else if (firstIndex == 1 && secondIndex == 2) {
+            return startTime.substring(0, startTime.length - 3) + 'p';
+        }
+
+        // 00:00 - 11:59
+        else {
+            return startTime.substring(0, startTime.length - 3) + 'a';
+        }
+    }
+
     // format string to M/D format
     formatStartDate(startDate) {
         return moment(startDate).format('M/D');
