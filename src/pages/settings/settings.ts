8846fde85b24c3a7e55c942bb0b4c89cdc0b987f
Ryan Koning <rkoning8@gmail.com>
2018-02-12 10:59:50 -0700

    settings page finally merged

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -11,32 +11,64 @@ import {DataProvider} from "../../providers/data-provider";
 })
 export class SettingsPage {
 	
+	selectedTab = "Categories"
+	
 	tags: Array<string>;
+	selectedTags: Array<string>;
+	tagDisplayValues: Array<boolean>;
+	
 	categories: Array<string>;
 	selectedCategories: Array<string>;
+	catDisplayValues: Array<boolean>;
 	categorySelect: any;
 	knobValues: any = {
 		upper: 50,
 		lower: 0
 	};
 	constructor(public dataProvider: DataProvider, public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
-/* 		this.storage.get('tags').then((data) => {
-			this.tags = data;
-		}); */
+ 		
+		this.selectedTags = new Array<string>();
+		this.storage.get('tags').then((data) => {
+			if (data != null) {
+				this.selectedTags = data;
+			} else {
+				this.selectedTags = [];
+			}
+		}); 
 		
 		this.tags = new Array<string>();
-		/*this.dataProvider.getTags()
+		this.tagDisplayValues = new Array<boolean>();
+		this.dataProvider.getTags()
 			 .subscribe(
 				  data => {
 						for (let i = 0; i < data.length; i++) {
 								
 							this.tags.push(data[i].TagName);
+							
+							for (let k = 0; k < this.tags.length; k++) {
+									if (this.selectedTags.includes(this.tags[k])) {
+										this.tagDisplayValues.push(true);
+									} else {
+										this.tagDisplayValues.push(false);
+									}
+								}
 						}
 				  },
 				  err => console.log(err)
 			 );
-			 */
-		this.categories = new Array<string>();        
+			 
+			 
+		this.selectedCategories = [];
+		
+		this.storage.get('categories').then((data)  => {
+			if (data != null) {
+				this.selectedCategories = data;
+			} else {
+				this.selectedCategories = [];
+			}			
+		});
+		this.categories = new Array<string>();
+		this.catDisplayValues = new Array<boolean>();        
 		
  		this.dataProvider.getCategories()
                 .subscribe(
@@ -45,61 +77,79 @@ export class SettingsPage {
                         for (let i = 0; i < data.length; i++) {
 									console.log(data[i]);
                            this.categories.push(data[i].CatName);
-									this.tags.push(data[i].CatName);
+									//this.tags.push(data[i].CatName);
                         }
+								
+								for (let k = 0; k < this.categories.length; k++) {
+									if (this.selectedCategories.includes(this.categories[k])) {
+										this.catDisplayValues.push(true);
+									} else {
+										this.catDisplayValues.push(false);
+									}
+								}
+								
 
                     },
                     err => console.log(err)
                 );
- 
-		this.selectedCategories = [];
 		
-		this.storage.get('categories').then((data)  => {
-			this.selectedCategories = data;
-			console.log(this.selectedCategories);
-		});
 		
 	}
 	
 	ionViewDidLoad()
 	{
-		
-	}
-	 
-	saveTag(val) {
-		let newTags = val.split(",");
-		this.storage.get('tags').then((data) => {
-			if (data != null) {
-				for(let k = 0; k < newTags.length; k++) {
-					data.push (newTags[k]);
-				}
-				this.storage.set('tags', data);
-			}
-			else {
-				let array = [];
-				for(let k = 0; k < newTags.length; k++) {
-					array.push (newTags[k]);
-				}
-				this.storage.set('tags', array);
-			}
-		});
-		
-		for(let k = 0; k < newTags.length; k++){
-			this.tags.push(newTags[k]);
-		}
+
+
 	}
 	
-	saveCategories(vals) {
-			
-			this.storage.get('categories').then((data) => {
-
-				let array = [];
-				for(let k = 0; k < vals.length; k++) {
-					array.push (vals[k]);
-				}
-				this.selectedCategories = array;
-				this.storage.set('categories', array);
-			
+	getCategoriesTab() {
+		this.selectedTab = "Categories";
+		
+	}
+	
+	getTagsTab() {
+		this.selectedTab = "Tags";
+	}
+	
+	getVenuesTab() {
+		this.selectedTab = "Venues";
+	}
+	
+	getAgeRangeTab() {
+		this.selectedTab = "Age Range";
+	}
+	
+	categoryChange(value) {
+		let index = this.selectedCategories.indexOf(value)
+		if (index > -1) {
+			this.selectedCategories.splice(index, 1);
+		} else {
+			this.selectedCategories.push(value);
+		}
+		
+		console.log(this.selectedCategories);
+	}
+	
+	saveCategories() {
+		this.storage.get('categories').then((data) => {
+			this.storage.set('categories', this.selectedCategories);
+		});
+	}
+	
+	tagChange(value) {
+		let index = this.selectedTags.indexOf(value)
+		if (index > -1) {
+			this.selectedTags.splice(index, 1);
+		} else {
+			this.selectedTags.push(value);
+		}
+		
+		console.log(this.selectedTags);
+	}
+	
+	saveTags() {
+		this.storage.get('tags').then((data) => {
+			this.storage.set('tags', this.selectedTags);
 		});
 	}
 	
