0da5a7077379b883f0e5cf9cf6df374159d09a7b
jordan.purinton <jordanomac@gmail.com>
2018-02-21 14:14:55 -0700

    Include planner page files.

---
+++ src/pages/planner/planner.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {IonicPage, NavParams} from 'ionic-angular';
+
+@IonicPage()
+@Component({
+    selector: 'page-planner',
+    templateUrl: 'planner.html',
+})
+export class PlannerPage {
+
+    constructor(public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+    }
+
+}
